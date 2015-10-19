4a6bdb9a4f8dfa9a20903ea453fab93fcaed6073
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-19 16:05:20 +0700

    init

---
+++ cli.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const npmUser = require('npm-user');
+
+const cli = meow(`
+	Usage
+	  $ npm-user <username>
+
+	Example
+	  $ npm-user sindresorhus
+	  Name: Sindre Sorhus
+	  Email: sindresorhus@gmail.com
+	  Homepage: http://sindresorhus.com
+	  Twitter: sindresorhus
+	  GitHub: sindresorhus
+	  Freenode: sindresorhus
+`);
+
+const username = cli.input[0];
+
+if (!username) {
+	console.error('Specify a npm username');
+	process.exit(1);
+}
+
+npmUser(username).then(user => {
+	const ret = [];
+
+	const createRow = (prefix, key) => {
+		if (user[key]) {
+			ret.push(`${prefix}: ${user[key]}`);
+		}
+	};
+
+	createRow('Name', 'name');
+	createRow('Email', 'email');
+	createRow('Homepage', 'homepage');
+	createRow('GitHub', 'github');
+	createRow('Twitter', 'twitter');
+	createRow('Freenode', 'freenode');
+
+	console.log(ret.join('\n'));
+});
---
+++ readme.md
@@ -0,0 +1,39 @@
+# npm-user-cli [![Build Status](https://travis-ci.org/sindresorhus/npm-user-cli.svg?branch=master)](https://travis-ci.org/sindresorhus/npm-user-cli)
+
+> Get user info of a npm user
+
+
+## Install
+
+```
+$ npm install --global npm-user-cli
+```
+
+
+## Usage
+
+```
+$ npm-user --help
+
+  Usage
+    $ npm-user <username>
+
+  Example
+    $ npm-user sindresorhus
+    Name: Sindre Sorhus
+    Email: sindresorhus@gmail.com
+    Homepage: http://sindresorhus.com
+    Twitter: sindresorhus
+    GitHub: sindresorhus
+    Freenode: sindresorhus
+```
+
+
+## Related
+
+- [npm-user](https://github.com/sindresorhus/npm-user) - API for this module
+
+
+## License
+
+MIT © [Sindre Sorhus](http://sindresorhus.com)
