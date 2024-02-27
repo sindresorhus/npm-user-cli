fb446567d7b2dd741f8ceb1bfa7885dac052c46b
tommy-mitchell <tmitchell7@uh.edu>
2024-02-27 01:41:13 -0600

    Prettify output, add graceful error handling

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 import process from 'node:process';
 import meow from 'meow';
 import npmUser from 'npm-user';
+import logSymbols from 'log-symbols';
 
 const cli = meow(`
 	Usage
@@ -24,19 +25,31 @@ if (!username) {
 	process.exit(1);
 }
 
-const user = await npmUser(username);
-const rows = [];
+try {
+	const user = await npmUser(username);
+	const rows = [];
 
-const createRow = (prefix, key) => {
-	if (user[key]) {
-		rows.push(`${prefix}: ${user[key]}`);
+	const createRow = (prefix, key) => {
+		if (user[key]) {
+			rows.push(`${logSymbols.info} ${prefix}: ${user[key]}`);
+		}
+	};
+
+	createRow('Name', 'name');
+	createRow('Email', 'email');
+	createRow('GitHub', 'github');
+	createRow('Twitter', 'twitter');
+
+	console.log(rows.join('\n'));
+} catch (error) {
+	// TODO:
+	// Or: error.response.status === 404
+	// Maybe `npm-user` should throw a new Error with code = 404?
+	if (error.message === 'User doesn\'t exist') {
+		console.error(`${logSymbols.error} User \`${username}\` could not be found.`);
+		process.exit(1);
 	}
-};
 
-createRow('Name', 'name');
-createRow('Email', 'email');
-createRow('GitHub', 'github');
-createRow('Twitter', 'twitter');
-
-console.log(rows.join('\n'));
+	throw error;
+}
 
